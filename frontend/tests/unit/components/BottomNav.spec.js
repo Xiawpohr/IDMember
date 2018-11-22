f039c20c2976c3354cb48b6e810005a5ebdb301b
XiawPohr <paradise121329@gmail.com>
2018-11-22 11:29:04 +0800

    fix BottomNav.spec

--- frontend/tests/unit/components/BottomNav.spec.js
+++ frontend/tests/unit/components/BottomNav.spec.js
@@ -1,11 +1,25 @@
 import { shallowMount } from '@vue/test-utils'
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import BottomNav from '@/components/BottomNav.vue'
 
+Vue.use(Vuex)
 Vue.use(Vuetify)
 
+const state = {
+  id: 1
+}
+
+const store = new Vuex.Store({
+  modules: {
+    auth: {
+      state
+    }
+  }
+})
+
 it('should render the component', () => {
-  const wrapper = shallowMount(BottomNav, { localVue: Vue })
+  const wrapper = shallowMount(BottomNav, { localVue: Vue, store })
   expect(wrapper.html()).toMatchSnapshot()
 })
